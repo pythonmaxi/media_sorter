d0fb30a7f912571eedf5bfd5a0e804ee1d110e2b
christoph prahl <christoph.prahl@dlr.de>
2021-01-07 16:21:31 +0100

    First version

---
+++ media_sort.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  1 17:31:40 2021
+
+@author: prah_ch
+"""
+import datetime
+import shutil
+import os
+import re
+from PIL import Image, ExifTags
+
+DATE_FMT = '%Y:%m:%d %H:%M:%S'
+IMG_REGEX = r'.*\.(jpe?g|png|raw)$'
+VID_REGEX = r'.*\.(mp4|mov)$'
+
+
+# %% Function to get date from an image
+def get_date(path):
+    """
+    Get the date of an image or video
+
+    Parameters
+    ----------
+    path : str
+        Path of the image or video.
+
+    Returns
+    -------
+    date : datetime.datetime
+        The creation time of the media file.
+
+    """
+    assert os.path.exists(path), f'{path} does not exists'
+    if re.match(IMG_REGEX, path, re.I):
+        img = Image.open(path)
+        exif = {ExifTags.TAGS[t]: v for t,
+                v in img._getexif().items() if t in ExifTags.TAGS}
+        date = datetime.datetime.strptime(exif['DateTime'], DATE_FMT)
+    elif re.match(VID_REGEX, path, re.I):
+        ctime = os.stat(path).st_ctime
+        date = datetime.datetime.fromtimestamp(ctime)
+    else:
+        print(f'{path} is not supported')
+        date = None
+    return date
+
+
+# %% Function to get all images in all subfolders of a directory
+def get_all_media(path, ignore_dirs=[]):
+    """
+    Search path recursively for videos and images
+
+    Parameters
+    ----------
+    path : str
+        Path to search for media.
+    ignore_dirs : list, optional
+        List of directories to ignore. The default is [].
+
+    Returns
+    -------
+    images : list
+        List of absolute media paths.
+
+    """
+    dir_content = os.listdir(path)
+    images = []
+    for file in dir_content:
+        abs_file = os.path.abspath(os.path.join(path, file))
+        if os.path.isdir(abs_file) and abs_file not in ignore_dirs:
+            images.extend(get_all_media(abs_file))
+            # TBD: Set recursion limit
+        elif re.match(IMG_REGEX, file) or re.match(VID_REGEX, path):
+            images.append(abs_file)
+    return images
+
+
+# %% Function to sort a list from get_all_media to a dictionary
+def sort_media_list(media_list, folder_format='%Y_%m'):
+    """
+    Sort media list created using get_all_media
+
+    Parameters
+    ----------
+    media_list : list
+        List of absolute media paths.
+    folder_format : str, optional
+        datetime.datetime.strftime format strig for
+        the folder names. The default is '%Y_%m'.
+
+    Returns
+    -------
+    out : dict
+        Sorted dictionary.
+
+    """
+    out = {}
+    for image in media_list:
+        date_str = get_date(image).strftime(folder_format)
+        if date_str not in out.keys():
+            out[date_str] = []
+        out[date_str].append(image)
+    return out
+
+
+# %% Function to prevent duplicate names
+def un_duplicate(name, used_names):
+    """
+    Un-Duplicate a filename
+
+    Parameters
+    ----------
+    name : str
+        Absolute path of the file.
+    used_names : list
+        List of files with same base-name.
+
+    Returns
+    -------
+    str
+        New absolute path with un-duplicated name.
+
+    """
+    base_name = os.path.basename(name)
+    cnt = 0
+    for used in used_names:
+        if re.match(r'.*_?' + base_name, os.path.basename(used)):
+            cnt += 1
+    un_dup_name = f'{cnt}_{base_name}'
+    return os.path.join(os.path.dirname(name), un_dup_name)
+
+
+# %% Function to copy/move sorted images
+def sort_dir(src, dst, overwrite=False, only_copy=True, ignore_dirs=[],
+             set_prc=lambda x: print(f'\rPercentage done: {x}', end=''), fld_fmt='%Y_%m'):
+    """
+    Sort a directory
+
+    Parameters
+    ----------
+    src : str
+        Source directory to sort.
+    dst : str
+        Destination directory for sorted media.
+    overwrite : bool, optional
+        Overwrite previous sort results?. The default is False.
+    only_copy : bool, optional
+        If true the source directory media will be moved
+        If false the source directory media will be only copied. The default is True.
+    ignore_dirs : list, optional
+        Directories to ignore. The default is [].
+    set_prc : function, optional
+        Function to set percentage.
+    fld_fmt : str, optional
+        New folder Format. The default is '%Y_%m'.
+
+    Returns
+    -------
+    None.
+
+    """
+    assert src != dst, f'{src} and {dst} are the same'
+    if src in dst:  # Check if dst is a subdir of src
+        ignore_dirs.append(os.path.abspath(dst))
+    im_list = get_all_media(src, ignore_dirs=ignore_dirs)
+    sorted_dict = sort_media_list(im_list, folder_format=fld_fmt)
+    im_count = len(im_list)
+    im_processed = 0
+    # %% Create folders if they don't exist
+    for folder in sorted_dict:
+        abs_path = os.path.join(dst, folder)
+        moved = []
+        if not os.path.exists(abs_path):
+            os.mkdir(abs_path)
+        # %% Move/copy images to folder
+        for image in sorted_dict[folder]:
+            new_path = os.path.join(dst, folder, os.path.basename(image))
+            if new_path in moved:
+                new_path = un_duplicate(new_path, moved)
+            moved.append(new_path)
+            if not overwrite and os.path.exists(new_path):
+                pass  # Simply do nothing since the counter should still go up
+            elif only_copy:
+                shutil.copy2(image, new_path)
+                # Use copy2 in attempt to copy metadata
+            else:
+                os.rename(image, new_path)
+            im_processed += 1
+            prc = im_processed / im_count * 100
+            set_prc(prc)
+
+
+# %% Run this if programm is called directly
+if __name__ == '__main__':
+    sort_dir('Source path', 'Destination path', overwrite=True)
---
+++ media_sort_gui.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  2 13:11:58 2021
+
+@author: prah_ch
+"""
+import sys
+import media_sort
+from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, \
+    QLabel, QLineEdit, QFileDialog, QCheckBox, QProgressBar
+from PyQt5.QtCore import QUrl, QSettings
+
+
+# %% Define the Main window
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QGridLayout()
+        self.setLayout(layout)
+        self._settings = QSettings('media_sorter', 'last-setings')
+        print(f'Using config file {self._settings.fileName()}')
+        # %% Source and destination
+        self._src_dir = QLineEdit()
+        self._dst_dir = QLineEdit()
+        layout.addWidget(QLabel('Source Directory:'), 0, 0)
+        layout.addWidget(QLabel('Destination'), 1, 0)
+        layout.addWidget(self._src_dir, 0, 1)
+        layout.addWidget(self._dst_dir, 1, 1)
+        # %% Browse path buttons
+        self._src_browse = QPushButton('Browse')
+        self._src_browse.clicked.connect(
+            lambda: self._get_dir(lambda x: self._src_dir.setText(x)))
+        self._dst_browse = QPushButton('Browse')
+        self._dst_browse.clicked.connect(
+            lambda: self._get_dir(lambda x: self._dst_dir.setText(x)))
+        layout.addWidget(self._src_browse, 0, 2)
+        layout.addWidget(self._dst_browse, 1, 2)
+        # %% Folder output format
+        layout.addWidget(QLabel('Output folder format'), 2, 0)
+        self._fld_fmt = QLineEdit()
+        HLink = 'https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes'
+        help_btn = QLabel(f'<a href="{HLink}">Help</a>')
+        help_btn.linkActivated.connect(self._open_link)
+        layout.addWidget(self._fld_fmt, 2, 1)
+        layout.addWidget(help_btn, 2, 2)
+        # %% Options
+        self._overwrite = QCheckBox('Overwrite')
+        self._only_copy = QCheckBox('Only copy images')
+        layout.addWidget(self._overwrite, 3, 0)
+        layout.addWidget(self._only_copy, 3, 1)
+        # %% The start button
+        self._start = QPushButton('Start')
+        self._start.clicked.connect(self._start_sort)
+        layout.addWidget(self._start, 3, 2)
+        # %% The progress bar
+        self._progress = QProgressBar()
+        self._progress.setValue(0)
+        layout.addWidget(self._progress, 4, 0, 1, 3)
+        # %% Set window Title
+        self.setWindowTitle('Media sorter')
+        # %% Restore last used values
+        self._src_dir.setText(self._settings.value('src', ''))
+        self._dst_dir.setText(self._settings.value('dst', ''))
+        self._overwrite.setChecked(self._settings.value('overwrite', False, type=bool))
+        self._only_copy.setChecked(self._settings.value('onlyCopy', True, type=bool))
+        self._fld_fmt.setText(self._settings.value('fldFmt', '%Y_%m'))
+
+    def _get_dir(self, set_fn):
+        directory = str(QFileDialog.getExistingDirectory())
+        set_fn(directory)
+
+    def _start_sort(self):
+        self._progress.setValue(0)
+        src = self._src_dir.text()
+        dst = self._dst_dir.text()
+        overwrite = self._overwrite.isChecked()
+        only_copy = self._only_copy.isChecked()
+        fld_fmt = self._fld_fmt.text()
+        media_sort.sort_dir(src, dst,
+                            overwrite=overwrite,
+                            only_copy=only_copy,
+                            set_prc=lambda x: self._progress.setValue(x),
+                            fld_fmt=fld_fmt)
+        # %% Write values to configuration
+        self._settings.setValue('src', src)
+        self._settings.setValue('dst', dst)
+        self._settings.setValue('overwrite', overwrite)
+        self._settings.setValue('onlyCopy', only_copy)
+        self._settings.setValue('fldFmt', fld_fmt)
+
+    def _open_link(self, link):
+        QDesktopServices.openUrl(QUrl(link))
+
+
+# %% Run this if programm is executed directly
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec_())
